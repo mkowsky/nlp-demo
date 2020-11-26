ae7c70cc5af9fdd6a5fed1a59edf8f8565debff3
Buarzej <blazejmp@o2.pl>
2020-11-26 02:21:58 +0100

    Metrics from shoe

---
+++ flask/MetricsProducer.py
@@ -0,0 +1,9 @@
+
+def getMetrics(classification_result, expected_result):
+    array_clas = np.array(classification_result)
+    array_exp = np.array(expected_result)
+    array_clas = np.where(array_clas == 'positive', '1', '0').astype('int32')
+    array_exp = np.where(array_exp == 'positive', '1', '0').astype('int32')
+    accuracy_score = metrics.accuracy_score(array_clas, array_exp)
+    tn, fp, fn, tp = confusion_matrix(array_exp, array_clas).ravel()
+    return [accuracy_score, tn, fp, fn, tp]
